798c20c9ac2fd24b396d9a62026887e299369af1
shamphone <shamphone@gmail.com>
2017-12-19 20:09:06 +0800

    add 73

--- user/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-function changeIframe() {
-    var id = $(this).attr('id')
-    $(this).addClass('is-active').siblings().removeClass("is-active")
-    $('.main--right iframe').attr('src', id )
-}
-$(document).ready(function() {
-    $('.nav__item').bind('click',changeIframe)
-});
